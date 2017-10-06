d90b91f654a6e40d444a9467b52ad121109e6d20
Unknown <eunicesazhneva@gmail.com>
2017-10-06 08:46:57 +0300

    Initial commit

---
+++ server/main.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import '../imports/api/tasks.js';
+import '../imports/api/users.js';
+import { Accounts } from 'meteor/accounts-base';
+
+Meteor.startup(() => {
+  // check(setTime
+  // code to run on server at startup
+});
+
+// Accounts.config({
+//     loginExpirationInDays: 0.000115741
+// })

775bb60902968a820bc194f0c0f5b5e19ac954df
eunicesazhneva <eunicesazhneva@gmail.com>
2017-10-17 10:00:50 +0300

    fix template

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,75 +1,104 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import { Meteor } from 'meteor/meteor';
-import { Tasks } from '../../api/tasks.js';
+import {Meteor} from 'meteor/meteor';
+import {Tasks} from '../../api/tasks.js';
 
 import template from './todosList.html';
 
 class TodosListCtrl {
-  constructor($scope) {
-    $scope.viewModel(this);
+    constructor($scope) {
+        $scope.viewModel(this);
 
-    this.subscribe('tasks');
+        this.subscribe('tasks');
 
-    this.hideCompleted = false;
+        this.hideCompleted = false;
 
-    this.helpers({
-      tasks() {
-        const selector = {};
+        this.helpers({
+            tasks() {
+                const selector = {};
 
-        // If hide completed is checked, filter tasks
-        if (this.getReactively('hideCompleted')) {
-          selector.checked = {
-            $ne: true
-          };
-        }
+                // If hide completed is checked, filter tasks
+                if (this.getReactively('hideCompleted')) {
+                    selector.checked = {
+                        $ne: true
+                    };
+                }
 
-        // Show newest tasks at the top
-        return Tasks.find(selector, {
-          sort: {
-            createdAt: -1
-          }
+                // Show newest tasks at the top
+                // return Tasks.find(selector, {
+                //   sort: {
+                //     createdAt: -1
+                //   }
+                // });
+
+                let groupedTasks = Tasks.find(selector, {
+                    sort: {
+                        createdAt: -1
+                    }
+                }).fetch();
+
+                console.log('Grouped Tasks:', groupedTasks);
+
+                const userNames = [];
+                const sortedTasks = [];
+                if (groupedTasks && groupedTasks.length) {
+                    for (const task of groupedTasks) {
+                        if (!userNames.find(uName => uName === task.username)) {
+                            userNames.push(task.username);
+                        }
+                    }
+                    for (const userName of userNames) {
+                        sortedTasks.push(groupedTasks.filter(task => task.username === userName));
+                    }
+                    console.log('Sorted Tasks:', sortedTasks);
+                    return sortedTasks;
+                }
+            },
+            incompleteCount() {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }).count();
+            },
+            currentUser() {
+                return Meteor.user();
+            }
         });
-      },
-      incompleteCount() {
-        return Tasks.find({
-          checked: {
-            $ne: true
-          }
-        }).count();
-      },
-      currentUser() {
-        return Meteor.user();
-      }
-    })
-  }
+        // console.log(Tasks.find({
+        //     checked: {
+        //         $ne: true
+        //     }
+        // }).count());
+    }
 
-  addTask(newTask) {
-    // Insert a task into the collection
-    Meteor.call('tasks.insert', newTask);
+    addTask(newTask) {
+        // Insert a task into the collection
+        Meteor.call('tasks.insert', newTask);
 
-    // Clear form
-    this.newTask = '';
-  }
+        // Clear form
+        this.newTask = '';
+    }
 
-  setChecked(task) {
-    // Set the checked property to the opposite of its current value
-    Meteor.call('tasks.setChecked', task._id, !task.checked);
-  }
+    setChecked(task) {
+        // Set the checked property to the opposite of its current value
+        Meteor.call('tasks.setChecked', task._id, !task.checked);
+    }
 
-  removeTask(task) {
-    Meteor.call('tasks.remove', task._id);
-  }
+    removeTask(task) {
+        Meteor.call('tasks.remove', task._id);
+    }
 
-  setPrivate(task) {
-    Meteor.call('tasks.setPrivate', task._id, !task.private);
-  }
+    setPrivate(task) {
+        Meteor.call('tasks.setPrivate', task._id, !task.private);
+    }
+
 }
 
 export default angular.module('todosList', [
-  angularMeteor
+    angularMeteor
 ])
-  .component('todosList', {
-    templateUrl: 'imports/components/todosList/todosList.html',
-    controller: ['$scope', TodosListCtrl]
-  });
+    .component('todosList', {
+        templateUrl: 'imports/components/todosList/todosList.html',
+        controller: ['$scope', TodosListCtrl]
+    });
